ebc96ab72b2f3cd4cf94d7d6b1a8960e3107b8ac
tiboth <37446840+tiboth@users.noreply.github.com>
2019-06-03 19:17:05 +0300

    added cors policy

--- controller/StatisiticController.py
+++ controller/StatisiticController.py
@@ -1,18 +1,15 @@
 import json
 
 from flask import request, Flask, Response
+from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api
 
 from service.PriceFluctuationService import PriceFluctuationService
 
 app = Flask(__name__)
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = ''
-# app.config['MYSQL_DB'] = 'licenta'
-# mysql = MySQL(app)
-
 api = Api(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app)
 
 
 class StatisticController(Resource):
@@ -20,25 +17,20 @@ class StatisticController(Resource):
     def __init__(self, service):
         self.price_fluctuation_service = service
 
-    # def getPriceForAdvertisement(self):
-    #     cur = mysql.connection.cursor()
-    #     cur.execute('''SELECT price FROM advertisement''')
-    #     prices = cur.fetchall()
-    #     return str(prices)
-
     def post(self):
         if request.headers['Content-Type'] == 'application/json':
             print("Request received")
             request_content = request.get_json()
-            print('nr_rooms:: ' + request_content['nr_rooms'])
-            print('old_building: ' + request_content['construction_year'])
-            print('new_building: ' + request_content['distributor'])
-            result = price_fluctuation_service.define_price_fluctuation(request_content['nr_rooms'],
-                                                                        request_content['construction_year'],
+            print('nr rooms:: ' + request_content['nrRooms'])
+            print('old building: ' + request_content['constructionYear'])
+            print('new building: ' + request_content['distributor'])
+            result = price_fluctuation_service.define_price_fluctuation(request_content['nrRooms'],
+                                                                        request_content['constructionYear'],
                                                                         request_content['distributor'])
             response = Response(response=json.dumps(result.__dict__),
                                 status=200,
                                 mimetype="application/json")
+
             return response
         else:
             return "Unsupported Media Type!"
--- dto/ResponseDto.py
+++ dto/ResponseDto.py
@@ -2,7 +2,7 @@
 
 class ResponseDto:
 
-    def __init__(self, dates, mean_price_list, preference_mean_price_list):
+    def __init__(self, dates, meanPriceList, preferenceMeanPriceList):
         self.dates = dates
-        self.mean_price_list = mean_price_list
-        self.preference_mean_price_list = preference_mean_price_list
+        self.meanPriceList = meanPriceList
+        self.preferenceMeanPriceList = preferenceMeanPriceList
